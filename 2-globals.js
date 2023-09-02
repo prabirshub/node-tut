9ba9b4947871537c31df4d22488ce05ebd6071a7
prabirshub <prabirsinghdigital@gmail.com>
2023-09-03 00:45:26 +0530

    nodejs tutorials

---
+++ 2-globals.js
@@ -0,0 +1,24 @@
+// GLOBALS - NO WINDOW !!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (common js)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+// console.log(process)
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(require)
+
+// setInterval(() => {
+//   console.log('hello world!')
+// }, 2000)
+
+// console.log(' 1st hello world!')
+
+// setTimeout(() => {
+//   console.log('2nd hello world!')
+// }, 2000)
+
+// console.log(' 3rd hello world!')
